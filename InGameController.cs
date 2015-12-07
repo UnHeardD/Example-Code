f296e7b8f27226ed50a2dbd651db8a5c025619e5
UnHeardD <unhearddrummer@gmail.com>
2015-12-07 12:58:04 +0000

    Snippets from current game
    This is an example of my work;
    
    MoveToLocation runs server side, handling all movement of players,
    communicating with MoveToLocationHandler.cs which is client side. The
    handler updates the player information client side as to the movement of
    other players, etc via InGameController.cs. This handles all information
    received for the player as their own information and other players
    surrounding them and initiating updates to the server too.

---
+++ InGameController.cs
@@ -0,0 +1,109 @@
+﻿public partial class InGameController : ViewController
+{
+    private static InGameController _instance;
+    public static InGameController Instance { get { return _instance; } }
+
+    private Dictionary<int, CObject> ObjectManager { get; set; }
+
+    public InGameController(View controlledView, byte subOperationCode = 0)
+        : base(controlledView, subOperationCode)
+    {
+        _instance = this;
+        ObjectManager = new Dictionary<int, CObject>();
+        EventSubCodeHandlers.Add((int)MessageSubCode.UserInfo, new UserInfoHandler(this));
+        EventSubCodeHandlers.Add((int)MessageSubCode.CharInfo, new CharInfoHandler(this));
+        EventSubCodeHandlers.Add((int)MessageSubCode.MoveToLocation, new MoveToLocationHandler(this));
+        EventSubCodeHandlers.Add((int)MessageSubCode.DeleteObject, new DeleteObjectHandler(this));
+        InitializeChat();
+        AddChatHandlers();
+        InitializeMovement();
+        AddMovementHandlers();
+    }
+
+    // LOCAL PLAYER
+    public void CreateOrUpdateUser(int objectId, UserInfo userInfo)
+    {
+        GameObject user = ObjectManager.ContainsKey(objectId) ? ObjectManager[objectId].gameObject : null;
+        if (user != null)
+        {
+            //UPDATE USER INFO WITH THE PACKET DATA
+            User.Health = userInfo.Health;
+        }
+        else
+        {
+            ControlledView.LogInfo("Instantiate User");
+            var userPrefab = Resources.Load("User");
+
+            var obj = (GameObject)Object.Instantiate(userPrefab, new Vector3(userInfo.Position.X, userInfo.Position.Y, userInfo.Position.Z),
+                                                        new Quaternion());
+            
+            obj.name = "" + objectId;
+            User = obj.GetComponent<CObject>();
+            User.ObjectId = objectId;
+
+            ///////////////////
+            // DEBUG INFO
+            ControlledView.LogInfo("Health: " + User.Health.ToString());
+            ///////////////////
+
+            ObjectManager.Add(objectId, User);
+        }
+    }
+
+    // NETWORK PLAYER
+    public void CreateOrUpdatePlayer(int objectId, CharInfo userInfo)
+    {
+        GameObject player = ObjectManager.ContainsKey(objectId) ? ObjectManager[objectId].gameObject : null;
+        if (player != null)
+        {
+            //UPDATE USER INFO WITH THE PACKET DATA
+        }
+        else
+        {
+            ControlledView.LogInfo("Instantiate Player");
+            var playerPrefab = Resources.Load("Player");
+            var obj = (GameObject)Object.Instantiate(playerPrefab, new Vector3(userInfo.Position.X, userInfo.Position.Y, userInfo.Position.Z),
+                                                        new Quaternion());
+            obj.name = "" + objectId;
+            var cobj = obj.GetComponent<CObject>();
+            cobj.FinalPosition = new Vector3(userInfo.Position.X, userInfo.Position.Y, userInfo.Position.Z);
+            ObjectManager.Add(objectId, cobj);
+        }
+    }
+
+    public void SendPlayerInGame()
+    {
+        Dictionary<byte, object> parameters = new Dictionary<byte,object>();
+
+        parameters.Add((byte)ClientParameterCode.SubOperationCode, MessageSubCode.PlayerInGame);
+
+        OperationRequest request = new OperationRequest { OperationCode = (byte)ClientOperationCode.Region, Parameters = parameters };
+
+        SendOperation(request, true, 0, true);
+    }
+
+    public void UpdatePosition(int objectId, MoveTo moveTo)
+    {
+        var cobj = ObjectManager.ContainsKey(objectId) ? ObjectManager[objectId] : null;
+        if (cobj != null)
+        {
+            cobj.UpdatePosition(new Vector3(moveTo.CurrentPosition.X, moveTo.CurrentPosition.Y, moveTo.CurrentPosition.Z), 
+                moveTo.Speed, moveTo.Moving, moveTo.Direction, moveTo.Facing, User.ObjectId == objectId);
+        }
+        ControlledView.LogInfo("UPDATEPOSITION BEING CALLED!");
+        ControlledView.LogInfo("Facccing: " + moveTo.Facing.ToString());
+        ControlledView.LogInfo("isUser: " + (User.ObjectId == objectId).ToString());
+    }
+
+    public void DeleteObject(int i)
+    {
+        //Add fade out alpha then delete object
+
+        if (ObjectManager.ContainsKey(i))
+        {
+            ControlledView.LogInfo("Deleting Object: " + i);
+            Object.Destroy(ObjectManager[i].gameObject);
+            ObjectManager.Remove(i);
+        }
+    }
+}
---
+++ MoveToLocationHandler.cs
@@ -0,0 +1,34 @@
+﻿public class MoveToLocationHandler : PhotonEventHandler
+{
+    public MoveToLocationHandler(ViewController controller)
+        : base(controller)
+    {
+
+    }
+
+    public override byte Code
+    {
+        get { return (byte)ClientEventCode.ServerPacket; }
+    }
+
+    public override int? SubCode
+    {
+        get { return (int)MessageSubCode.MoveToLocation; }
+    }
+
+    public override void OnHandleEvent(EventData eventData)
+    {
+        //////////
+        // DEBUG INFO
+        //////////
+        _controller.ControlledView.LogInfo("MoveToLocationHandler - Handled MoveTo");
+        InGameController controller = _controller as InGameController;
+        if (controller != null)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(MoveTo));
+            StringReader inStream = new StringReader((string)eventData.Parameters[(byte)ClientParameterCode.Object]);
+            controller.UpdatePosition((int)eventData.Parameters[(byte)ClientParameterCode.ObjectId],
+                ((MoveTo)serializer.Deserialize(inStream)));
+        }
+    }
+}
